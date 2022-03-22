268ceb02e8ba03795fb359dcf9b6026bdb0f00fb
Andrea055 <vivanco11.ac@gmail.com>
2022-03-22 01:21:53 +0100

    add dialog on image

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -1,6 +1,7 @@
 <script>
   import Gallery from '/static/components/gallery.svelte'
   import item from '/static/images/slide/list.json'
+  import { dialogs } from "svelte-dialogs";
   function open(e) {
 		window.open(e.detail.src)
 	}
@@ -10,25 +11,18 @@
 <link href="/dist/css/lightbox.css" rel="stylesheet" />
 <script src="/dist/js/lightbox.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"></script>
-<script>
-UIkit.lightbox(element).show(0);
-
-</script>
 </svelte:head>
 
 <!--<Gallery on:click={open}>
 <img src={"/images/slide/" + image } />
 </Gallery>-->
-
-<Gallery>
+<Gallery on:click={(event) => dialogs.alert('<img src=' + event.detail.src +  ' alt="Placeholder Image" />')}>
 {#each item as image}
-
-          <img src={"/images/slide/" + image }  alt="" >  
+          <img src={"/images/slide/" + image }  alt="" />  
  
-	
+
 {/each}
 </Gallery>
-
 <style>
 
 </style>
