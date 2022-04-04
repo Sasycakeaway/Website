b4d3088aff01767c98856ecabba9bafe6b71150c
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-04-04 23:24:15 +0200

    fix frollacacao

--- src/routes/frollacacao.svelte
+++ src/routes/frollacacao.svelte
@@ -15,13 +15,11 @@
 <h1>&nbsp;</h1>
 <div class="divv" align="center">
   <div
-    class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin"
-    uk-grid
+    class="uk-card uk-card-default"
+    
   >
-    <div class="uk-card-media-left uk-cover-container">
-      <img src="/images/cropan.webp" alt="" uk-cover />
-      <canvas width="600" height="400" />
-    </div>
+    <hr>
+    <img src="/images/cropan.webp" alt="" class="imgcenter"/>
     <div>
       <div class="uk-card-body">
         <p class="det" style="color:black;">
