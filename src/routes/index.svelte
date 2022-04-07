31f6d7d564c3f62c08b74aac35d366de3b2d2521
Andrea055 <andrea.canale@istitutoagnelli.it>
2022-04-08 00:02:18 +0200

    add contatti

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
   import Head from "/static/components/head.svelte";
   import Prod from "/static/components/prod.svelte";
- 
+  import SvelteSeo from "svelte-seo";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getPerformance} from "firebase/performance"
@@ -40,7 +40,68 @@ const perf = getPerformance(app);
   <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/grids-responsive-min.css" />
   
 </svelte:head>
-
+<SvelteSeo
+ jsonLd={
+    {
+      "@context": "https://schema.org",
+      "@type": "Restaurant",
+      "image": [
+        "https://www.sasycakeaway.com/images/slide/Biscotti/Sasy_Macaron1.jpeg",
+        "https://www.sasycakeaway.com/images/slide/Torte/Sasy_creamtart01.jpeg",
+        "https://www.sasycakeaway.com/images/slide/Uova/WhatsApp%20Image%202021-03-26%20at%2001.18.52.jpeg",
+        "https://www.sasycakeaway.com/images/slide/Pasta%20di%20zucchero/IMG-20200614-WA0020.jpg"
+       ],
+      "name": "Sasy's Cake Away",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Viale falchera 69",
+        "addressLocality": "Torino",
+        "addressRegion": "IT",
+        "postalCode": "10156",
+        "addressCountry": "IT"
+      },
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "5",
+          "bestRating": "5"
+        },
+        "author": {
+          "@type": "Person",
+          "name": "Sabrina Vitrani"
+        }
+      },
+      "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": 45.1263404,
+        "longitude": -7.7086123
+      },
+      "url": "https://www.sasycakeaway.com/",
+      "telephone": "+3339273367",
+      "servesCuisine": "Pasticceria",
+      "priceRange": "$$",
+      "openingHoursSpecification": [
+        {
+          "@type": "OpeningHoursSpecification",
+          "dayOfWeek": [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+          ],
+          "opens": "10:00",
+          "closes": "12:00"
+        },
+      ],
+      "menu": "https://www.sasycakeaway.com/gallery",
+      "acceptsReservations": "True"
+    }
+ }
+/>
 <Head />
 <h5>&nbsp;</h5>
 <div class="prodi">
