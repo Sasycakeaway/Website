ad399411b80865170b22b9a1ebaa5b371d6e178d
Andrea055o <vivanco11.ac@gmail.com>
2022-06-25 16:36:08 +0200

    fix background color

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
 // svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import { defineConfig, searchForWorkspaceRoot } from 'vite';
 export default {
   kit: {
-    vite: {
-      fs: {
-        allow: [searchForWorkspaceRoot(process.cwd()),'/Users/andreacanale/Desktop/my-app/static/components/']
-      }
-    },
     trailingSlash: 'always',
     adapter: adapter(),
 
